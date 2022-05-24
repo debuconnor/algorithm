b3a7ac52c452ca75e3fd48d34939ebafd8da91f7
debuconnor <gterppw@gmail.com>
2022-05-24 18:25:21 +0900

    fixed 15829

--- acmicpc.net/15829/main.go
+++ acmicpc.net/15829/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"math"
 	"os"
 )
 
@@ -12,25 +11,21 @@ func main(){
 	w := bufio.NewWriter(os.Stdout)
 	defer w.Flush()
 
-	const FIX = 96
 	var l, r, m, result uint64
 	var str string
 
-	r = 31
+	r = 1
 	m = 1234567891
 	result = 0
 
 	fmt.Fscan(reader, &l)
 	fmt.Fscan(reader, &str)
 
-	for i, v := range str{
-		result += ((uint64(v) - FIX) * powInt(r, uint64(i))) % m
+	for _, v := range str{
+		result = (result + (uint64(v - 'a') + 1) * r) % m
+	  //result += (랜덤숫자 * r) % 1234567891
+		r = (r * 31) % m
 	}
 
 	fmt.Fprintln(w, result)
-	fmt.Fprintln(w, powInt(r, 49))
-}
-
-func powInt(x, y uint64) uint64 {
-    return uint64(math.Pow(float64(x), float64(y)))
 }
