1c10d3d77e0353b1abe39a9c00d6d9046e7a1a27
debuconnor <gterppw@gmail.com>
2022-05-26 15:54:30 +0900

    fixed 4949

--- acmicpc.net/4949/main.go
+++ acmicpc.net/4949/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
+	"strings"
 )
 
 func main(){
@@ -10,5 +12,47 @@ func main(){
 	w := bufio.NewWriter(os.Stdout)
 	defer w.Flush()
 
-	var str 
+	var result []string
+
+	for {
+		str, _ := r.ReadString('\n')
+		str = strings.ReplaceAll(str, "\n", "")
+		if str == "." {
+			break
+		}
+
+		var stack []string
+
+		for _, v := range str{
+			top := len(stack) - 1
+			char := string(v)
+
+			if top == -1 && (v == ')' || v == ']'){
+				stack = append(stack, " ")
+				break
+			}
+
+			if v == '(' || v == '['{
+				stack = append(stack, char)
+			} else if v == ')' {
+				if stack[top] == "("{
+					stack = stack[:top]
+				}
+			} else if v == ']' {
+				if stack[top] == "["{
+					stack = stack[:top]
+				}
+			}
+		}
+
+		if len(stack) == 0{
+			result = append(result, "yes")
+		} else {
+			result = append(result, "no")
+		}
+	}
+
+	for _, v := range result{
+		fmt.Fprintln(w, v)
+	}
 }
